a618d4b05d1abc010e7566eb6533f05611bbafec
Marat <gaimarat@dca90482b6a5.ant.amazon.com>
2019-04-05 18:08:49 -0700

    cleaning

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
-/* Theme Name: Marcus | A Responsive HTML5 Resume Template
-   Author: Shreethemes
-   Version: 1.0.0
-   Created: Fabruary 2019
-   File Description: Main JS file of the template
-*/
-
 (function ($) {
 
     'use strict';
@@ -25,18 +18,6 @@
         offset: 20
     });
 
-	// Magnific Popup
-    $('.mfp-image').magnificPopup({
-        type: 'image',
-        closeOnContentClick: true,
-        mainClass: 'mfp-fade',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1]
-        }
-    });
-    
 	//owlCarousel
     $("#owl-demo").owlCarousel({
         autoPlay: 15000, //Set AutoPlay to 3 seconds
@@ -44,10 +25,6 @@
         itemsDesktop: [1199, 2],
         itemsDesktopSmall: [979, 2]
     });
-
-    // Easy Pie Chart
-    $('.chart').easyPieChart();
-
 
     // Show video after loaded
     setTimeout( () => $(".video-overlay").addClass("hide"), 3000)
