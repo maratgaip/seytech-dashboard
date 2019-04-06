df7d6f3ddb0be25ce407edbc22a351e546b959fb
Marat <gaimarat@dca90482b6a5.ant.amazon.com>
2019-04-05 19:46:59 -0700

    t

--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@ $to = new SendGrid\Email(null, "marattig@example.com");
 $content = new SendGrid\Content("text/plain", $comments);
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-$apiKey = getenv('SENDGRID_API_KEY');
+$apiKey = SENDGRID_API_KEY;
 $sg = new \SendGrid($apiKey);
 
 $response = $sg->client->mail()->send()->post($mail);
