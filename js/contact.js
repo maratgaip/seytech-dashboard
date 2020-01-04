4cea051f127ac36517852e4ccd7957c09b3ed978
Marat Gaipov <gaimarat@amazon.com>
2020-01-04 03:54:17 -0800

    enroll

--- js/contact.js
+++ js/contact.js
@@ -31,3 +31,36 @@
         return false;
 
     });
+    // Working Contact Form
+    $('#enroll-form').submit(function() {
+        var action = $(this).attr('action');
+
+        $("#message").slideUp(750, function() {
+            $('#message').hide();
+
+            $('#submit')
+                .before('')
+                .attr('disabled', 'disabled');
+
+            $.post(action, {
+                    name: $('#name').val() +' '+ $('#lastname').val(),
+                    email: $('#email').val(),
+                    phone: $('#phone').val(),
+                    comments: '<div><strong>' + 'Type: ' + '</strong>' + $('#type').val() + '</div><div><strong>' + 'City: ' + '</strong>' + $('#city').val() + '</div><div><strong>' + 'Phone: ' + '</strong>' + $('#phone').val() + '</div><div><strong>' + 'Message:</strong></div>' + $('#comments').val()
+                },
+                function(data) {
+                    document.getElementById('message').innerHTML = data;
+                    $('#message').slideDown('slow');
+                    $('#cform img.contact-loader').fadeOut('slow', function() {
+                        $(this).remove()
+                    });
+                    $('#submit').removeAttr('disabled');
+                    if (data.match('success') != null) $('#cform').slideUp('slow');
+                }
+            );
+
+        });
+
+        return false;
+
+    });
